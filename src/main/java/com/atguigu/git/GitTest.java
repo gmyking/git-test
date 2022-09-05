73f1bc1cb1dcaf427c031ed1973a59651f83bb6b
gmyking <2663454637@qq.com>
2022-09-05 14:35:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @auther 郭明远~
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
